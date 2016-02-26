1b8b4618afee7ee622c557f790b793ee929fcbb0
Ian Webster <ianw_git@ianww.com>
2016-02-26 11:23:15 -0800

    Working on pending submission stuff

--- astrometry/models.py
+++ astrometry/models.py
@@ -1,10 +1,9 @@
 from __future__ import unicode_literals
 
-import datetime
-
 import django
 from django.contrib import admin
 from django.db import models
+from django.utils import timezone
 from jsonfield import JSONField
 
 from astrometry_client import Client
@@ -13,15 +12,17 @@ class AstrometrySubmission(models.Model):
     SUBMITTED = 'SUBMITTED'
     COMPLETE = 'COMPLETE'
     FAILED_TO_SUBMIT = 'FAILED_TO_SUBMIT'
+    FAILED_TO_PROCESS = 'FAILED_TO_PROCESS'
     SUBMISSION_STATUSES = (
         (SUBMITTED, 'Submitted'),
         (COMPLETE, 'Complete'),
         (FAILED_TO_SUBMIT, 'Failed to submit'),
+        (FAILED_TO_PROCESS, 'Failed to process'),
     )
     subid = models.IntegerField()
     status = models.CharField(
             max_length=50, choices=SUBMISSION_STATUSES, default=SUBMITTED)
-    created_at = models.DateTimeField(default=django.utils.timezone.now)
+    created_at = models.DateTimeField(default=timezone.now)
     succeeded_at = models.DateTimeField(blank=True, null=True)
 
     def get_astrometry_net_url(self):
@@ -37,7 +38,8 @@ class AstrometrySubmission(models.Model):
 
         # TODO(ian) Update related AstrometrySubmissionJobs.
 
-admin.site.register(AstrometrySubmission)
+    def __str__(self):
+        return 'Sub #%d - %s' % (self.subid, self.status)
 
 class AstrometrySubmissionJob(models.Model):
     SUCCESS = 'SUCCESS'
@@ -47,9 +49,20 @@ class AstrometrySubmissionJob(models.Model):
         (UNKNOWN, 'Unknown'),
     )
 
+    jobid = models.IntegerField()
     submission = models.ForeignKey(
             AstrometrySubmission, on_delete=models.CASCADE)
     status = models.CharField(
             max_length=50, choices=JOB_STATUSES, default=UNKNOWN)
+    created_at = models.DateTimeField(default=timezone.now)
     succeeded_at = models.DateTimeField(blank=True, null=True)
+
+    # API results from astrometry.net.
     annotations = JSONField(blank=True, null=True)
+    info = JSONField(blank=True, null=True)
+
+    def __str__(self):
+        return 'Job #%d for Sub #%d' % (self.jobid, self.submission.subid)
+
+admin.site.register(AstrometrySubmission)
+admin.site.register(AstrometrySubmissionJob)
--- process_pending_submissions.py
+++ process_pending_submissions.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 
-import datetime
 import json
 import os
 import sys
 
 import django
 from django.conf import settings
+from django.utils import timezone
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 sys.path.insert(0, os.getcwd())
 django.setup()
 
-from astrometry.models import AstrometrySubmission
+from astrometry.models import AstrometrySubmission, AstrometrySubmissionJob
 from astrometry.astrometry_client import Client
 
 client = Client()
@@ -31,26 +32,33 @@ for submission in pending_submissions:
         num_success = 0
         for job_id in job_ids:
             info = client.send_request('jobs/%d/info' % (job_id))
-            if info['status'] == 'solving':
+            status = info['status']
+            if status == 'solving':
                 print '-> Job %d is still solving' % (job_id)
                 continue
-            elif info['status'] != 'success':
-                print '-> Warning: unsuccessful job %d for submission %d' \
-                        % (job_id, submission.subid)
+            elif status == 'failure':
+                print '-> Job %d has failed' % (job_id)
+                submission.status = AstrometrySubmission.FAILED_TO_PROCESS
+                submission.save()
+                continue
+            elif status != 'success':
+                print '-> Warning: unknown status %s: job %d, submission %d' \
+                        % (status, job_id, submission.subid)
                 continue
 
-            annotations = json.dumps(client.send_request('jobs/%d/annotations' \
-                    % (job_id)))
+            annotations = client.send_request('jobs/%d/annotations' % (job_id))
 
             # Save these results.
             job = AstrometrySubmissionJob.objects.create(
                     submission=submission,
+                    jobid=job_id,
                     status=AstrometrySubmissionJob.SUCCESS,
-                    annotations=annotations)
+                    annotations=annotations,
+                    info=info)
             num_success += 1
 
         if num_success == len(job_ids):
             # Update submission.
-            submission.succeeded_at = datetime.datetime.now()
+            submission.succeeded_at = timezone.now()
             submission.status = AstrometrySubmission.COMPLETE
             submission.save()
