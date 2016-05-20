17603adb4186ed175311e804b8ccc6d2b8c56ceb
Ian Webster <ianw_git@ianww.com>
2016-05-19 19:04:45 -0700

    fix label again

--- point_source_extraction.py
+++ point_source_extraction.py
@@ -92,8 +92,7 @@ def compute_psf_flux(image_data, sources, \
         print 'Saving bar chart...'
         plt.close('all')
         plt.bar(xrange(len(computed_fluxes)), computed_fluxes)
-        plt.ylabel('Count')
-        plt.xlabel('Flux')
+        plt.ylabel('Flux')
         plt.savefig(bar_output_path)
 
     if residual_path:
