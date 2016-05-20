322b5ebdfcb0cbadb1cd37f6d60ab0df2a41b57a
Ian Webster <ianw_git@ianww.com>
2016-05-19 23:40:48 -0700

    Use logging module

--- process_pending_submissions.py
+++ process_pending_submissions.py
@@ -2,6 +2,7 @@
 
 import argparse
 import json
+import logging
 import os
 import sys
 import urllib
@@ -19,6 +20,9 @@ import imageflow.s3_util as s3_util
 from astrometry.models import AstrometrySubmission, AstrometrySubmissionJob
 from astrometry.astrometry_client import Client
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 class SubmissionHandler():
 
     def __init__(self, client, submission, args):
@@ -30,34 +34,34 @@ class SubmissionHandler():
         client = self.client
         submission = self.submission
 
-        print 'Querying for submission %d...' % (submission.subid)
+        logger.info('Querying for submission %d...' % (submission.subid))
         substatus = client.sub_status(submission.subid, True)
 
         if not (substatus and 'processing_finished' in substatus):
-            print 'Submission is not done submitting yet.'
+            logger.info('Submission is not done submitting yet.')
             return False
 
         job_ids = substatus['jobs']
-        print 'Submission has processing jobs: %s' % (job_ids)
+        logger.info('Submission has processing jobs: %s' % (job_ids))
         num_success = 0
         for job_id in job_ids:
             info = client.send_request('jobs/%d/info' % (job_id))
 
             status = info['status']
             if status == 'solving' or status == 'processing':
-                print '-> Job %d is still solving' % (job_id)
+                logger.info('-> Job %d is still solving' % (job_id))
                 # TODO(ian): Sometimes an image can get stuck in this state; there
                 # should be a maximum timeout.
                 return False
             elif status == 'failure':
-                print '-> Job %d has failed' % (job_id)
+                logger.warn('-> Job %d has failed' % (job_id))
                 submission.status = AstrometrySubmission.FAILED_TO_PROCESS
                 submission.save()
                 return False
             elif status != 'success':
-                print '-> Warning: unknown status %s: job %d, submission %d' \
-                        % (status, job_id, submission.subid)
-                print '-> Got the following response:', info
+                logger.warn('-> Warning: unknown status %s: job %d, submission %d' \
+                        % (status, job_id, submission.subid))
+                logger.warn('-> Got the following response: %s' % info)
                 return False
 
             annotations = client.send_request('jobs/%d/annotations' % (job_id))
@@ -70,7 +74,7 @@ class SubmissionHandler():
                     annotations=annotations,
                     info=info)
             num_success += 1
-            print '-> Job %d was added' % (job_id)
+            logger.info('-> Job %d was added' % (job_id))
 
         if num_success > 0 and num_success == len(job_ids):
             self.process_completed_submission(job)
@@ -80,7 +84,7 @@ class SubmissionHandler():
     def process_completed_submission(self, job):
         submission = self.submission
 
-        print '-> Submission %d, Job %d is complete' % (submission.subid, job.jobid)
+        logger.info('-> Submission %d, Job %d is complete' % (submission.subid, job.jobid))
 
         # Save results.
         self.save_submission_results(job)
@@ -94,7 +98,7 @@ class SubmissionHandler():
     def save_submission_results(self, job):
         submission = self.submission
 
-        print '-> Uploading results for submission %d' % (submission.subid)
+        logger.info('-> Uploading results for submission %d' % (submission.subid))
 
         annotated_display_url = 'http://nova.astrometry.net/annotated_display/%d' \
                 % (job.jobid)
@@ -108,24 +112,24 @@ class SubmissionHandler():
 
         # Annotated jpg.
         name = '%d_%d_annotated.jpg' % (submission.subid, job.jobid)
-        print '  -> Uploading', name, '...'
+        logger.info('  -> Uploading %s...' % name)
         if not args.dry_run:
             s3_util.upload_to_s3_via_url(annotated_display_url, upload_key_prefix, name)
 
         # CORR.
         name = '%d_%d_corr.fits' % (submission.subid, job.jobid)
-        print '  -> Uploading', name, '...'
+        logger.info('  -> Uploading %s...' % name)
         if not args.dry_run:
             s3_util.upload_to_s3_via_url(corr_url, upload_key_prefix, name)
 
         # FITS.
         name = '%d_%d_image.fits' % (submission.subid, job.jobid)
         fits_image_data = urllib.urlopen(new_image_fits_url).read()
-        print '  -> Uploading', name, '...'
+        logger.info('  -> Uploading %s...' % name)
         if not args.dry_run:
             s3_util.upload_to_s3(fits_image_data, upload_key_prefix, name)
 
-        print '-> Uploaded results for submission %d' % (submission.subid)
+        logger.info('-> Uploaded results for submission %d' % (submission.subid))
 
         # Point source extraction processing.
         self.process_fits_image(fits_image_data, job, upload_key_prefix)
@@ -133,7 +137,7 @@ class SubmissionHandler():
     def process_fits_image(self, image_data, job, upload_key_prefix):
         submission = self.submission
 
-        print '-> Processing fits image for submission %d' % (submission.subid)
+        logger.info('-> Processing fits image for submission %d' % (submission.subid))
 
         data = point_source_extraction.load_data_as_fits(image_data)
         sources = point_source_extraction.compute(data)
@@ -141,19 +145,19 @@ class SubmissionHandler():
         # Coords.
         coords_plot_path = '%d_%d_plot.png' % (submission.subid, job.jobid)
         point_source_extraction.plot(sources, data, coords_plot_path)
-        print '  -> Uploading', coords_plot_path, '...'
+        logger.info('  -> Uploading %s...' % coords_plot_path)
         if not args.dry_run:
             s3_util.upload_to_s3_via_file(coords_plot_path, upload_key_prefix)
 
         coords_fits_path = '%d_%d_coords.fits' % (submission.subid, job.jobid)
         point_source_extraction.save_fits(sources, coords_fits_path)
-        print '  -> Uploading', coords_fits_path, '...'
+        logger.info('  -> Uploading %s...' % coords_fits_path)
         if not args.dry_run:
             s3_util.upload_to_s3_via_file(coords_fits_path, upload_key_prefix)
 
         coords_json_path = '%d_%d_coords.json' % (submission.subid, job.jobid)
         point_source_extraction.save_json(sources, coords_json_path)
-        print '  -> Uploading', coords_json_path, '...'
+        logger.info('  -> Uploading %s...' % coords_json_path)
         if not args.dry_run:
             s3_util.upload_to_s3_via_file(coords_json_path, upload_key_prefix)
 
@@ -165,10 +169,10 @@ class SubmissionHandler():
         point_source_extraction.compute_psf_flux(data, sources, \
                 psf_scatter_path, psf_bar_path, psf_hist_path, psf_residual_path)
 
-        print '  -> Uploading', psf_scatter_path, '...'
-        print '  -> Uploading', psf_bar_path, '...'
-        print '  -> Uploading', psf_hist_path, '...'
-        print '  -> Uploading', psf_residual_path, '...'
+        logger.info('  -> Uploading %s' % psf_scatter_path)
+        logger.info('  -> Uploading %s' % psf_bar_path)
+        logger.info('  -> Uploading %s' % psf_hist_path)
+        logger.info('  -> Uploading %s' % psf_residual_path)
         if not args.dry_run:
             s3_util.upload_to_s3_via_file(psf_scatter_path, upload_key_prefix)
             s3_util.upload_to_s3_via_file(psf_bar_path, upload_key_prefix)
@@ -178,7 +182,7 @@ class SubmissionHandler():
         # TODO(ian): Should delete the files afterwards, or create them as
         # temporary files.
 
-        print '-> Processed fits image for submission %d' % (submission.subid)
+        logger.info('-> Processed fits image for submission %d' % (submission.subid))
 
 def get_args():
     parser = argparse.ArgumentParser('Process outstanding jobs')
