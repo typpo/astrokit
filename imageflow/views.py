fa0dfb85db83e8540fb4b7eaab577d35a580d2c5
Ian Webster <ianw_git@ianww.com>
2016-05-24 23:18:25 -0700

    api endpoint for submission results

--- imageflow/views.py
+++ imageflow/views.py
@@ -1,11 +1,15 @@
 import time
 
 from django.conf import settings
+from django.http import JsonResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 import imageflow.s3_util as s3_util
 from astrometry.util import process_astrometry_online
+
+from astrometry.models import AstrometrySubmission, AstrometrySubmissionJob
+from imageflow.models import AnalysisResult
 
 def index(request):
     return render_to_response('index.html')
@@ -21,5 +25,8 @@ def upload_image(request):
     return render_to_response('upload_image.html', {},
             context_instance=RequestContext(request))
 
-def api_get_submission_results(request):
-    return 'foo'
+def api_get_submission_results(request, subid):
+    results = AnalysisResult.objects.filter(astrometry_job__submission__subid=subid)
+    result_urls = [result.astrometry_annotated_display_url for result in results]
+
+    return JsonResponse({'msg': result_urls})
