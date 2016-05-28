ef28965d0660c88c9f6a22a2e3f14fc736fa40a1
Ian Webster <ianw_git@ianww.com>
2016-05-28 11:32:16 -0700

    basic submission pending page

--- imageflow/views.py
+++ imageflow/views.py
@@ -1,6 +1,7 @@
 import time
 
 from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import JsonResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
@@ -26,17 +27,26 @@ def upload_image(request):
             context_instance=RequestContext(request))
 
 def view_submission(request, subid):
-    result = AnalysisResult.objects.get(astrometry_job__submission__subid=subid)
+    try:
+        result = AnalysisResult.objects.get(astrometry_job__submission__subid=subid)
+    except ObjectDoesNotExist:
+        return render_to_response('submission_pending.html', {},
+                context_instance=RequestContext(request))
 
     template_args = {
         'result': result.get_summary_obj(),
     }
-
     return render_to_response('submission.html', template_args,
             context_instance=RequestContext(request))
 
 def api_get_submission_results(request, subid):
-    result = AnalysisResult.objects.get(astrometry_job__submission__subid=subid)
+    try:
+        result = AnalysisResult.objects.get(astrometry_job__submission__subid=subid)
+    except ObjectDoesNotExist:
+        return JsonResponse({
+            'success': False,
+            'message': 'Result not found.',
+        })
 
     return JsonResponse({
         'success': True,
