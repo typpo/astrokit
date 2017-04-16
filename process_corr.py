70a648788a0913adbdb90c4627d499b037b5e4fd
Ian Webster <ianw_git@ianww.com>
2017-04-15 22:01:02 -0700

    compute differential magnitude (and basic form of standard magnitude)

--- process_corr.py
+++ process_corr.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import math
+
 from astropy.io import fits
 from astroquery.simbad import Simbad
 from astroquery.vizier import Vizier
@@ -8,16 +10,16 @@ from astroquery.vizier import Vizier
 # https://groups.google.com/forum/#!topic/astrometry/Lk1LuhwBBNU
 im = fits.open('/home/ian/Downloads/corrtest.fits')
 data = im[1].data
-radec_pairs = zip(data['index_ra'], data['index_dec'])
+radec_pairs = zip(data['index_ra'], data['index_dec'], data['FLUX'])
 
 simbad = Simbad()
 # http://astroquery.readthedocs.io/en/latest/simbad/simbad.html
 # http://simbad.u-strasbg.fr/simbad/sim-help?Page=sim-fscript#VotableFields
 # http://simbad.u-strasbg.fr/simbad/sim-id?protocol=html&Ident=Wolf%201061&output.format=VOTable
 simbad.add_votable_fields('fluxdata(V)', 'flux_unit(mag)', 'flux_system(mag)')
-print len(radec_pairs)
-for ra, dec in radec_pairs[:30]:
-    print 'ra, dec:', ra, dec
+comparison_objs = []
+for ra, dec, flux in radec_pairs[:10]:
+    print 'ra, dec, flux:', ra, dec, flux
     '''
     result = simbad.query_region('%f %f' % (ra, dec))
     if result:
@@ -31,4 +33,30 @@ for ra, dec in radec_pairs[:30]:
         continue
     r2mag = results[0]['R2mag'].data[0]
     desig = results[0]['USNO-B1.0'].data[0]
-    print desig, r2mag
+    # print desig, r2mag
+
+    comparison_objs.append({
+        'designation': desig,
+        'reference_Rmag': r2mag,
+        'observed_flux': flux,
+    })
+
+print comparison_objs
+
+target = comparison_objs[0]
+comparisons = comparison_objs[1:]
+
+diff_sum = 0
+for comparison in comparisons:
+    instrumental_mag_target = -2.5 * math.log10(target['observed_flux'])
+    instrumental_mag_comparison = -2.5 * math.log10(comparison['observed_flux'])
+
+    # Very basic standard magnitude formula from Brian Warner.
+    mag_target = (instrumental_mag_target - instrumental_mag_comparison) + comparison['reference_Rmag']
+    print 'computed', mag_target, 'vs actual', target['reference_Rmag']
+
+    diff_sum += instrumental_mag_target - instrumental_mag_comparison
+
+# Compute differential magnitude.
+target_diff_magnitude = diff_sum / len(comparisons)
+print 'target diff magnitude:', target_diff_magnitude
