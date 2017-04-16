bed6f192668922e896ced5ac13f7b39cad1aa6f2
Ian Webster <ianw_git@ianww.com>
2017-04-15 21:25:18 -0700

    use usno catalog

--- process_corr.py
+++ process_corr.py
@@ -2,6 +2,7 @@
 
 from astropy.io import fits
 from astroquery.simbad import Simbad
+from astroquery.vizier import Vizier
 # from astropy.coordinates import Angle
 
 # https://groups.google.com/forum/#!topic/astrometry/Lk1LuhwBBNU
@@ -16,11 +17,18 @@ simbad = Simbad()
 simbad.add_votable_fields('fluxdata(V)', 'flux_unit(mag)', 'flux_system(mag)')
 print len(radec_pairs)
 for ra, dec in radec_pairs[:30]:
-    print ra, dec
+    print 'ra, dec:', ra, dec
+    '''
     result = simbad.query_region('%f %f' % (ra, dec))
     if result:
         print result['MAIN_ID'].data, result['FLUX_V'].data
+    '''
 
-# v = Vizier(columns=['_RAJ2000', '_DEJ2000','B-V', 'Vmag', 'Plx'])
-# v.query_region('7.4527083453 -33.3246888886', radius='1s')
-
+    # Query USNO catalog.
+    v = Vizier(columns=['_RAJ2000', '_DEJ2000','B-V', 'R2mag', 'B2mag', 'USNO-B1.0'])
+    results = v.query_region('%f %f' % (ra, dec), radius='1s', catalog='I/284/out')
+    if len(results) < 1:
+        continue
+    r2mag = results[0]['R2mag'].data[0]
+    desig = results[0]['USNO-B1.0'].data[0]
+    print desig, r2mag
