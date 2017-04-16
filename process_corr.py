919d5e4aed218f5fead96e615d58d986723e93fe
Ian Webster <ianw_git@ianww.com>
2017-04-16 11:36:33 -0700

    rename and simplify things

--- process_corr.py
+++ process_corr.py
@@ -4,6 +4,8 @@ import math
 import os
 import shelve
 
+import numpy as np
+
 from astropy.io import fits
 from astroquery.simbad import Simbad
 from astroquery.vizier import Vizier
@@ -69,30 +71,33 @@ for i in range(len(comparison_objs)):
     target = comparisons[i]
     del comparisons[i]
 
-    diff_sum = 0
-    mag_targets = []
+    comparison_diffs = 0
+    target_mags = []
     for comparison in comparisons:
         # TODO(ian): Really should be ADU/Exposure
-        instrumental_mag_target = -2.5 * math.log10(target['observed_flux'])
+        instrumental_target_mag = -2.5 * math.log10(target['observed_flux'])
         instrumental_mag_comparison = -2.5 * math.log10(comparison['observed_flux'])
 
         # Very basic standard magnitude formula from Brian Warner.
-        mag_target = (instrumental_mag_target - instrumental_mag_comparison) + comparison['reference_Rmag']
-        mag_targets.append(mag_target)
-        print 'computed', mag_target, 'vs actual', target['reference_Rmag']
+        target_mag = (instrumental_target_mag - instrumental_mag_comparison) + comparison['reference_Rmag']
+        target_mags.append(target_mag)
+        print 'computed', target_mag, 'vs actual', target['reference_Rmag']
 
-        diff_sum += instrumental_mag_target - instrumental_mag_comparison
+        comparison_diffs += instrumental_target_mag - instrumental_mag_comparison
 
     # Compute differential magnitude.
-    target_diff_magnitude = diff_sum / len(comparisons)
+    target_diff_magnitude = np.mean(comparison_diffs)
     print 'target diff magnitude:', target_diff_magnitude
 
-    target_mag_avg = sum(mag_targets) / len(comparisons)
+    target_mag_avg = np.mean(target_mags)
+    target_mag_std = np.std(target_mags)
     print 'mag target average:', target_mag_avg, 'vs actual', target['reference_Rmag']
+    print 'mag target std:', target_mag_std
+
     percent_error = abs(target['reference_Rmag'] - target_mag_avg) / target['reference_Rmag'] * 100.0
     percent_errors.append(percent_error)
     print '  --> difference:', (target_mag_avg - target['reference_Rmag'])
     print '  --> % error:', percent_error
 
 print '*' * 80
-print 'avg percent error:', (sum(percent_errors) / len(percent_errors))
+print 'avg percent error:', np.mean(percent_errors)
