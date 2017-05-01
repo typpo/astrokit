1e7f1d0b00e133793036f13481c91f62a8f027af
Ian Webster <ianw_github@ianww.com>
2017-05-01 16:03:59 +0300

    Update quick photometry test to be more portable

--- quick_photometry_test.sh
+++ quick_photometry_test.sh
@@ -1,10 +1,25 @@
 #!/bin/bash -e
 
-./point_source_extraction.py \
-    --coords_json coords.json \
-    'https://s3.amazonaws.com/astrokit-uploads/processed/1251219/1474609162-1251219_1738733_image.fits'
+mkdir -p test_output
 
-./compute_apparent_magnitudes.py \
-    --corr_fits ~/Downloads/1474609156-1251219_1738733_corr.fits \
+pushd test_output
+
+if [[ ! -f ./coords.json ]]; then
+  wget -O coords.json 'https://s3.amazonaws.com/astrokit-uploads/processed/1078914/1464154928-1078914_1558980_coords.json'
+fi
+if [[ ! -f ./corr.fits ]]; then
+wget -O corr.fits 'https://s3.amazonaws.com/astrokit-uploads/processed/1078914/1464154875-1078914_1558980_corr.fits'
+fi
+if [[ ! -f ./image.fits ]]; then
+  wget -O image.fits 'https://s3.amazonaws.com/astrokit-uploads/processed/1078914/1464154893-1078914_1558980_image.fits'
+fi
+
+../point_source_extraction.py \
+    --coords_json ./coords.json \
+    ./image.fits
+
+../compute_apparent_magnitudes.py \
+    --corr_fits ./corr.fits \
     --point_source_json ./coords.json
 
+popd
