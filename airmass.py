cc883b67e05a49821985c71bdd8c3e46b061cedd
Ian Webster <ianw_github@ianww.com>
2017-10-17 22:49:47 +0300

    airmass calculation

--- airmass.py
+++ airmass.py
@@ -1,18 +1,22 @@
 from datetime import datetime, timedelta
 
+import numpy as np
+from astropy import units as u
+from astropy.time import Time
+from astropy.coordinates import SkyCoord, EarthLocation, AltAz
+
 J2000_UNIX_TIMESTAMP = datetime.fromtimestamp(946727930.816)
 
-def compute_airmass(latitude, longitude, ra, dec, obs_date):
-    # Useful reference: http://www.stargazing.net/kepler/altaz.html#twig02
-    days_from_j2000 = \
-            (obs_date - J2000_UNIX_TIMESTAMP).total_seconds() / timedelta(days=1).total_seconds()
+def compute_airmass(latitude, longitude, elevation, ra, dec, obs_time):
+    coord = SkyCoord(ra * u.deg, dec * u.deg, frame='icrs')
+    loc = EarthLocation(lat=latitude * u.deg, lon=longitude * u.deg, height=elevation)
+    time = Time(obs_time.isoformat())
 
-    # This approximation is within 0.3 seconds of time for dates within 100
-    # years of J2000.
-    local_sidereal_time = 100.46 + 0.985647 * d + longitude + 15 * UT
+    altaz = coord.transform_to(AltAz(obstime=time, location=loc))
+    return altaz.secz
 
 def test():
-    compute_airmass(None, None, None, datetime.fromtimestamp(1508268879))
+    compute_airmass(42, -42, 0, 30, 30, datetime.fromtimestamp(1508268879))
 
 if __name__ == '__main__':
     test()
