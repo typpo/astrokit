ba43c249ce4f18e3fb319e045262d918db62c42e
Ian Webster <ianw_github@ianww.com>
2017-10-21 11:34:27 +0300

    minor changes to tf api

--- transformation_coefficient.py
+++ transformation_coefficient.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 from imageflow.s3_util import upload_to_s3
 
-def compute_tf_from_analysis(analysis):
+def compute_tf_from_analysis(analysis, graph_output_path):
     apparent_mags = []
     standard_mags = []
     colors_1 = []
@@ -14,23 +14,29 @@ def compute_tf_from_analysis(analysis):
         colors_1.append(star[analysis.color_index_1.urat1_key])
         colors_2.append(star[analysis.color_index_2.urat1_key])
 
-    return compute_tf(apparent_mags, standard_mags, colors_1, colors_2)
+    tf, (xs, ys, A, c) = compute_tf(apparent_mags, standard_mags, colors_1, colors_2)
 
-def compute_tf(apparent_mags, standard_mags, colors_1, colors_2, graph_output_path=None):
+    tf_graph_url = None
+    if graph_output_path:
+        plt.plot(xs, ys, '+', label='Original data', markersize=10)
+        plt.plot(xs, m*xs + c, 'r', label='Fitted line')
+        plt.xlabel('%s - %s' % (analysis.color_index_1.band , analysis.color_index_2.band))
+        plt.ylabel('M - m')
+        plt.legend()
+        plt.savefig(graph_output_path)
+
+        # TODO(ian): Upload graph to s3
+
+    return tf, tf_graph_url
+
+def compute_tf(apparent_mags, standard_mags, colors_1, colors_2):
     # Reference: https://docs.scipy.org/doc/numpy-1.13.0/reference/generated/numpy.linalg.lstsq.html
     xs = np.array(standard_mags) - np.array(apparent_mags)
     ys = np.array(colors_1) - np.array(colors_2)
     A = np.vstack([xs, np.ones(len(xs))]).T
     m, c = np.linalg.lstsq(A, ys)[0]
 
-    if graph_output_path:
-        plt.plot(xs, ys, '+', label='Original data', markersize=10)
-        plt.plot(xs, m*xs + c, 'r', label='Fitted line')
-        plt.legend()
-        plt.savefig(graph_output_path)
-        # TODO(ian): Upload graph to s3
-
-    return m
+    return m, (xs, ys, A, c)
 
 def test():
     compute_tf([0, .5, 1, 1.5],
