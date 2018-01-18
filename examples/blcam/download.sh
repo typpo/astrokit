320034a805ea9e117b91a35967c37fa7554caa5f
Ian Webster <ianw_git@ianww.com>
2018-01-17 22:50:27 -0800

    blcam downloads to its own directory

--- examples/blcam/download.sh
+++ examples/blcam/download.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+pushd $(dirname $0) &>/dev/null
+
 for i in {1..9}; do
   wget "http://www.phys.vt.edu/~jhs/SIP/images/blcam/blcam_0${i}.fit"
 done
@@ -7,3 +9,5 @@ done
 for i in {10..21}; do
   wget "http://www.phys.vt.edu/~jhs/SIP/images/blcam/blcam_${i}.fit"
 done
+
+popd &>/dev/null
