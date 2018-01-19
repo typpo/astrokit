2576886d04dceb2c295ccbb522ca6a28f746b28a
Ian Webster <ianw_git@ianww.com>
2018-01-18 23:17:00 -0800

    Link from lightcurve to submission page #33

--- lightcurve/views.py
+++ lightcurve/views.py
@@ -5,6 +5,8 @@ from django.template import RequestContext
 from imageflow.models import ImageAnalysis, UserUploadedImage
 from lightcurve.models import LightCurve
 
+from process_pending_submissions import SubmissionHandler
+
 def edit_lightcurve(request, lightcurve_id):
     lc = LightCurve.objects.get(id=lightcurve_id)
     images = UserUploadedImage.objects.filter(lightcurve=lc)
@@ -14,3 +16,7 @@ def edit_lightcurve(request, lightcurve_id):
     }
     return render_to_response('lightcurve.html', context,
             context_instance=RequestContext(request))
+
+def create_analysis(request, uploaded_image_id):
+    # Placeholder function for triggering the creation of an analysis.
+    pass
