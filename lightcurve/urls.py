e012648e738bc7c20faf4bf9d370803b01a0f498
Ian Webster <ianw_git@ianww.com>
2018-01-20 00:04:32 -0800

    remove unused submit_astrometry route

--- lightcurve/urls.py
+++ lightcurve/urls.py
@@ -2,5 +2,4 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('lightcurve.views',
     url(r'^edit/(?P<lightcurve_id>[0-9]+)$', 'edit_lightcurve', name='edit_lightcurve'),
-    url(r'^submit_astrometry/(?P<uploaded_image_id>[0-9]+)$', 'submit_astrometry', name='submit_astrometry'),
 )
--- lightcurve/views.py
+++ lightcurve/views.py
@@ -16,17 +16,3 @@ def edit_lightcurve(request, lightcurve_id):
     }
     return render_to_response('lightcurve.html', context,
             context_instance=RequestContext(request))
-
-def submit_astrometry(request, uploaded_image_id):
-    image = UserUploadedImage.objects.get(id=uploaded_image_id)
-    if hasattr(image, 'submission'):
-        return JsonResponse({
-            'success': True,
-            'subid': image.submission.id,
-        })
-    image.submission = process_astrometry_online(image.image_url)
-    image.save()
-    return JsonResponse({
-        'success': True,
-        'subid': image.submission.id,
-    })
