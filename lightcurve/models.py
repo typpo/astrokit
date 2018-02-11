a3637f82a84b92596c94b86455a3d2f486f3de61
Ian Webster <ianw_git@ianww.com>
2018-02-11 09:00:41 -0800

    remove image_filter attr on lightcurve model

--- lightcurve/models.py
+++ lightcurve/models.py
@@ -11,10 +11,6 @@ class LightCurve(models.Model):
 
     name = models.CharField(max_length=1024)
 
-    image_filter = models.ForeignKey(ImageFilter,
-                                     related_name='lightcurve_image_filter_set',
-                                     default=ImageFilter.objects.get_default())
-
     magband = models.ForeignKey(ImageFilter,
                                 related_name='lightcurve_magband_set',
                                 default=ImageFilter.objects.get_default())
