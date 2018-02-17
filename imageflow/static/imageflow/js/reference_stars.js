f1f1ed823353445f399d44398ba6ecf9c2ff6a13
Ian Webster <ianw_git@ianww.com>
2018-02-17 00:48:03 -0800

    Add UI for comparison star selection

--- imageflow/static/imageflow/js/reference_stars.js
+++ imageflow/static/imageflow/js/reference_stars.js
@@ -3,4 +3,27 @@ $(function() {
                        'instrumental',
                        window.catalogData,
                        window.catalogData);
+
+  $('td input').on('change', function() {
+    var $this = $(this);
+    var checked = $this.is(':checked');
+    var $tr = $this.parent().parent();
+    //$tr.toggleClass('success', checked);
+    var starId = $tr.data('star-id');
+    $('.js-reference-table')
+        .find('tr[data-star-id="' + starId + '"]').toggleClass('success', checked)
+        .find('input').attr('checked', true)
+  });
+
+  // Link both reference star lists together.
+  var scrollTimer = null;
+  $('.table-wrapper').on('scroll', function() {
+    var me = this;
+    var $this = $(this);
+    clearTimeout(scrollTimer);
+    setTimeout(function() {
+      $('.table-wrapper').not(me).scrollTop($this.scrollTop());
+    }, 100);
+    return false;
+  });
 });
