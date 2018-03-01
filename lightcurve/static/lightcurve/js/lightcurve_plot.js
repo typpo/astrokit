1d9da9955ac73dabf74c01e5cc34419acaef0418
Ian Webster <ianw_github@ianww.com>
2018-03-01 14:22:33 -0800

    Ability to fold the light curve plot interactively on plot page. Closes #99

--- lightcurve/static/lightcurve/js/lightcurve_plot.js
+++ lightcurve/static/lightcurve/js/lightcurve_plot.js
@@ -1,16 +1,37 @@
+var plotConfig = {
+  period: null,
+  lastResults: null,
+};
+
 function loadData() {
   var url = '/lightcurve/' + window.lightcurveId +
             '/plot_json?type=' + getChartType();
   $.get(url, function(data) {
-    console.log(data.results);
-    plot(data.results);
+    plotConfig.lastResults = data.results;
+    plot();
   });
+}
+
+function setupListeners() {
+  $('#lightcurve-period').on('change', function() {
+    plotConfig.period = $(this).val() || null;
+    plot();
+  });
+}
+
+function getTimestampVal(result) {
+  if (plotConfig.period) {
+    // Fold the lightcurve, if applicable.
+    // https://www.southampton.ac.uk/~sdc1g08/BinningFolding.html
+    return (result.timestampJd / plotConfig.period) % 1;
+  }
+  return result.timestampJd;
 }
 
 function getChartForStandard(results) {
   return [
     {
-      x: results.map(function(r) { return r.timestampJd }),
+      x: results.map(getTimestampVal),
       y: results.map(function(r) { return r.result.mag_standard }),
       error_y: {
         type: 'data',
@@ -26,7 +47,7 @@ function getChartForStandard(results) {
 function getChartForInstrumental(results) {
   return [
     {
-      x: results.map(function(r) { return r.timestampJd }),
+      x: results.map(getTimestampVal),
       y: results.map(function(r) { return r.result.mag_instrumental }),
       type: 'scatter',
       mode: 'markers',
@@ -37,6 +58,13 @@ function getChartForInstrumental(results) {
 function getChart(results) {
   return getChartType() === 'instrumental' ?
           getChartForInstrumental(results) : getChartForStandard(results);
+}
+
+function getChartXAxisLabel() {
+  if (plotConfig.period) {
+    return 'Phase';
+  }
+  return 'Time (Julian Date)';
 }
 
 function getChartYAxisLabel() {
@@ -51,12 +79,12 @@ function getChartType() {
   return url.searchParams.get('type') || 'standard';
 }
 
-function plot(results) {
-  var chart = getChart(results);
+function plot() {
+  var chart = getChart(plotConfig.lastResults);
   var layout = {
     title: window.lightcurveName,
     xaxis: {
-      title: 'Time (Julian Date)',
+      title: getChartXAxisLabel(),
       exponentformat: 'none',
       separatethousands: false,
       hoverformat: '.5f',
@@ -73,4 +101,5 @@ function plot(results) {
 
 $(function() {
   loadData();
+  setupListeners();
 });
